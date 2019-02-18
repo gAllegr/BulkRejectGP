0d6ab6d68fcf612a3978f78e3fcc56da9b89ccad
gAllegr <allegretta92@hotmail.it>
2019-02-18 20:20:13 +0100

    Search term enconding

--- bulkrejectgp.py
+++ bulkrejectgp.py
@@ -4,7 +4,7 @@ from marionette_driver.marionette import Marionette
 from marionette_driver import By, Wait
 import subprocess
 import argparse
-from urllib.parse import quote_plus
+import urllib
 
 def sendmessage(title, message):
     subprocess.Popen(['notify-send', title, message])
@@ -79,7 +79,8 @@ except:
 # Move to the term
 term = args.search
 # encode the term and change the space ('%20') in a '+'
-term = quote_plus(term, safe='')
+term = urllib.quote_plus(term, safe='')
+
 client.navigate("https://translate.wordpress.org/consistency?search=" + term + "&set=" + args.lang + "%2Fdefault")
 # Remove the strings different from our
 removeOtherStrings = "var right = document.querySelectorAll('table td:nth-child(2) .string');for (var i=0; i<right.length; i++){if(right[i].innerHTML!=='" + args.remove.replace("'","\\'") + "') {td = right[i].parentNode;tr = td.parentNode;tr.outerHTML=''}}"
